7f93bfb3d23c4d5ef55f888e4a28f4b7f2da29ee
turtledev <turtledev65@gmail.com>
2024-04-06 10:08:05 +0300

    refactor: make the callout component more generic

--- src/components/callout.tsx
+++ src/components/callout.tsx
@@ -1,5 +1,5 @@
-import Button from "./button";
-const Callout = (props: { backgroundImg?: string }) => {
+import { PropsWithChildren } from "react";
+const Callout = (props: { backgroundImg?: string } & PropsWithChildren) => {
   return (
     <div className="block">
       {props.backgroundImg &&
@@ -8,11 +8,7 @@ const Callout = (props: { backgroundImg?: string }) => {
           <div className="overlay overlay--purple" />
         </>
       }
-      <span className="title title--xl">Secure an Easy Way to Trade Online</span>
-      <p>Sign up today to receive your free digital wallet. You can immediately start buying or selling Bitcoin, hassle-free.</p>
-      <div className="mt-2">
-        <Button>Get Started</Button>
-      </div>
+      {props.children}
     </div>
   );
 }
