78f9832a40886aa5f5bd54b2cab3ead017a51dc9
turtledev <turtledev65@gmail.com>
2024-04-12 22:44:40 +0300

    chore: add name prop to currency card and change priceStatus to priceChangePercent

--- src/components/currency-card.tsx
+++ src/components/currency-card.tsx
@@ -4,25 +4,26 @@ import Graph from "./graph"
 
 type Props = {
   currPrice: string | number;
+  name: string;
   symbol: string;
   icon: string;
   graphData: number[];
-  percentStatus: number;
+  priceChangePercent: number;
   variant?: "green" | "blue" | "purple";
 }
-const CurrencyCard = ({ currPrice, symbol, icon, graphData, percentStatus, variant = "green" }: Props) => {
+const CurrencyCard = ({ currPrice, name, symbol, icon, graphData, priceChangePercent, variant = "green" }: Props) => {
   return (
     <Card className="currency-card" variant={variant}>
       <div className="currency-card__icon">
         <img src={icon} />
       </div>
       <div className="currency-card__info" >
-        <span>Bitcoin</span>
+        <span>{name}</span>
         <span className="currency-card__info__symbol">{symbol}</span>
       </div>
       <div className="currency-card__price">{currPrice}</div>
       <div className="currency-card__status">
-        <span className={`badge badge--${variant}`}>{percentStatus >= 0 ? "+" : "-"}{percentStatus}</span>
+        <span className={`badge badge--${variant}`}>{priceChangePercent >= 0 && "+"}{priceChangePercent}%</span>
         <Graph data={graphData} variant={variant} />
       </div>
 
@@ -31,3 +32,4 @@ const CurrencyCard = ({ currPrice, symbol, icon, graphData, percentStatus, varia
 }
 
 export default CurrencyCard;
+
