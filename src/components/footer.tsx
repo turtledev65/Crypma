0b0f7692f792ecfdf34b5177014402615127911b
turtledev <turtledev65@gmail.com>
2024-04-13 00:08:44 +0300

    refactor: make the footer use the new css classes instead of title and remove the container

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,38 +3,36 @@ import Logo from "./logo";
 
 const Footer = () => {
   return (
-    <div className="container mb-3">
-      <footer className="footer">
-        <div className="footer__section">
-          <Logo />
-        </div>
-        <div className="footer__section">
-          <p className="title">Exchange</p>
-          <ul>
-            <li><a className="link" href="#">Buy crypto</a></li>
-            <li><a className="link" href="#">Market</a></li>
-            <li><a target="_blank" className="link" href="https://en.wikipedia.org/wiki/Cryptocurrency">Learn crypto</a></li>
-            <li><a className="link" href="#">Contact</a></li>
-          </ul >
-        </div >
-        <div className="footer__section">
-          <p className="title">Contact</p>
-          43252 Borer Mountains
-          <br />
-          Zackerychester
-          <br />
-          Bahamas
-          <br />
-          <br />
-          732-528-4945
-        </div >
-        <div className="footer__section">
-          <p className="title">Newsletter</p>
-          Subscribe our newsletter to get tips and more information about Crypma
-          <EmailInput className="mt-1" onSubmit={(email) => console.log(`${email} subscribed to newsletter`)} />
-        </div >
-      </footer >
-    </div>
+    <footer className="footer">
+      <div className="footer__section">
+        <Logo />
+      </div>
+      <div className="footer__section">
+        <p className="font-bold color-white">Exchange</p>
+        <ul>
+          <li><a className="link" href="#">Buy crypto</a></li>
+          <li><a className="link" href="#">Market</a></li>
+          <li><a target="_blank" className="link" href="https://en.wikipedia.org/wiki/Cryptocurrency">Learn crypto</a></li>
+          <li><a className="link" href="#">Contact</a></li>
+        </ul >
+      </div >
+      <div className="footer__section">
+        <p className="font-bold color-white">Contact</p>
+        43252 Borer Mountains
+        <br />
+        Zackerychester
+        <br />
+        Bahamas
+        <br />
+        <br />
+        732-528-4945
+      </div >
+      <div className="footer__section">
+        <p className="font-bold color-white">Newsletter</p>
+        Subscribe our newsletter to get tips and more information about Crypma
+        <EmailInput className="mt-1" onSubmit={(email) => console.log(`${email} subscribed to newsletter`)} />
+      </div >
+    </footer >
   )
 }
 
