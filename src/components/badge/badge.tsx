ccbebe63ea824b3c7bcbfce2aa10cbc4571b95b1
turtledev <turtledev65@gmail.com>
2024-04-14 21:51:46 +0300

    refactor: move the badge styles to a css module

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -1,9 +1,10 @@
 import { PropsWithChildren } from "react";
+import styles from "./badge.module.css";
 
 type Props = { variant?: ColorVariant } & PropsWithChildren;
 
 const Badge = ({ variant = "green", children }: Props) => {
-  return <span className={`badge badge--${variant}`}>{children}</span>
+  return <span className={`${styles.badge} ${styles[variant]}`}>{children}</span>
 }
 
 export default Badge;
