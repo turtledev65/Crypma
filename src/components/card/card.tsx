482f9a0e65ea9310360e980680ac37c6e8769f06
turtledev <turtledev65@gmail.com>
2024-04-16 23:24:32 +0300

    style: add a fancy hover animation to cards

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useRef } from "react";
 import styles from "./card.module.css"
 
 type Props = {
@@ -7,8 +7,25 @@ type Props = {
 } & PropsWithChildren;
 
 const Card = ({ variant = "blue", children, className }: Props) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleHover = (e: React.PointerEvent<HTMLDivElement>) => {
+    if (!ref.current) return;
+
+    const rect = ref.current.getBoundingClientRect();
+    const centerX = rect.x + (rect.right - rect.x) / 2;
+    const centerY = rect.y + (rect.bottom - rect.y) / 2;
+    const dir = {
+      x: Math.sign(e.clientX - centerX),
+      y: Math.sign(e.clientY - centerY),
+    }
+
+    ref.current.style.setProperty("--shadow-offset-x", `${dir.x * 2}px`);
+    ref.current.style.setProperty("--shadow-offset-y", `${dir.y * 2}px`);
+  }
+
   return (
-    <div className={`${styles.card} ${styles[variant]} ${className}`}>
+    <div onPointerMove={handleHover} ref={ref} className={`${styles.card} ${styles[variant]} ${className}`}>
       {children}
     </div>
   );
